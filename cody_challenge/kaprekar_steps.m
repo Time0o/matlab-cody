a8e7b715c57565ab618510e3fb48d38495191ef4
Timo Nicolai <timonicolai@arcor.de>
2019-03-30 13:24:31 +0100

    Small style fix

--- cody_challenge/kaprekar_steps.m
+++ cody_challenge/kaprekar_steps.m
@@ -4,7 +4,7 @@ function y = kaprekar_steps(x)
     while x ~= 6174
         digits = sprintf('%04s', num2str(x));
 
-        x_desc = str2double(sort(digits, 'Descend'));
+        x_desc = str2double(sort(digits, 'descend'));
         x_asc = str2double(sort(digits));
 
         x = x_desc - x_asc;
